a67803ef5fdbe33d6c49a6c12cb13a43111df0ff
Michael Petrov <imrealpunk@gmail.com>
2017-02-11 04:01:22 +0300

    DELETE bindings

--- src/controllers/UpdateCardController.js
+++ src/controllers/UpdateCardController.js
@@ -2,17 +2,27 @@ import BindingModel from '../model/Bind.model';
 import * as requests from '../lib/requests';
 
 export const updateCardController = (req, res, next) => {
-  const {data} = req.body.action;
+  const {data, date, username, memberCreator} = req.body.action;
 
   return BindingModel.getBindedCards({idCard: data.card.id})
     .then(binding =>
-      Promise.all(
-        binding.reduce((promises, bind) =>
+      Promise.all([
+        ...binding.reduce((promises, bind) =>
+          promises.concat(BindingModel.createOrUpdateBinding({
+            action: 'edit',
+            date,
+            idCard: bind.idBindedCard,
+            enabled: !bind.closed,
+            userNameLastSynced: memberCreator.username,
+            userLastSynced: memberCreator.id
+          })), []),
+
+        ...binding.reduce((promises, bind) =>
           promises.concat(requests.updateCard({
             card: data.card,
             id: bind.idBindedCard,
             token: res.user.trelloToken
           })), [])
-      )
+      ])
     )
 };
